9f4e434de766540329e1563a8295df8f9b8c3ae2
Marian Sigler <m@qjym.de>
2015-01-19 01:24:02 +0100

    backend: add option to runserver to run in a subdir

--- backend/zwl/runserver.py
+++ backend/zwl/runserver.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python2
-
+# -*- coding: utf8 -*-
+import sys
 from zwl import app
-app.run(debug=True, port=8231)
+
+if len(sys.argv) > 1 and sys.argv[1] == 'subdir':
+
+    # allows testing if everything still works when not running under /
+    from werkzeug.serving import run_simple
+    from werkzeug.wsgi import DispatcherMiddleware
+
+    application = DispatcherMiddleware(None, {
+        '/zwl': app
+    })
+
+    run_simple('localhost', 8232, application, use_debugger=True)
+
+else:
+    app.run(debug=True, port=8231)
