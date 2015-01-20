d34e4faaf99d9f09b75bc02ce5a3c3d980e9d4dd
Marian Sigler <m@qjym.de>
2015-01-20 16:44:41 +0100

    backend: subdir server: add proper 404 reponse outside /zwl

--- backend/zwl/runserver.py
+++ backend/zwl/runserver.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 import os
 import sys
+from flask import Response
 from zwl import app
 
 extra_files = []
@@ -12,16 +13,18 @@ except KeyError:
 
 if len(sys.argv) > 1 and sys.argv[1] == 'subdir':
 
+    notfound = Response('Use <a href="/zwl/">/zwl/</a>, please!', status=404)
+
     # allows testing if everything still works when not running under /
     from werkzeug.serving import run_simple
     from werkzeug.wsgi import DispatcherMiddleware
 
-    application = DispatcherMiddleware(None, {
-        '/zwl': app
+    application = DispatcherMiddleware(notfound, {
+        '/zwl': app,
     })
 
     run_simple('localhost', 8232, application, use_debugger=True,
-               extra_files=extra_files)
+               use_reloader=True, extra_files=extra_files)
 
 else:
     app.run(debug=True, port=8231, extra_files=extra_files)
