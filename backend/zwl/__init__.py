3ed0537a5e16cad0abac09cbb77229b0f180ad97
Marian Sigler <m@qjym.de>
2015-01-26 03:53:22 +0100

    backend: rename settings to default_settings

--- backend/zwl/__init__.py
+++ backend/zwl/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf8 -*-
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from zwl import settings
+from zwl import default_settings
 
 app = Flask(__name__)
 
-app.config.from_object(settings)
+app.config.from_object(default_settings)
 app.config.from_envvar('ZWL_SETTINGS', silent=True)
 db = SQLAlchemy(app)
 
