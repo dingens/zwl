afb76d08ed2b1393ac7f292894b00522b17053d3
Marian Sigler <m@qjym.de>
2015-02-04 16:23:21 +0100

    backend: run in threaded mode

--- backend/zwl/runserver.py
+++ backend/zwl/runserver.py
@@ -24,7 +24,7 @@ if len(sys.argv) > 1 and sys.argv[1] == 'subdir':
     })
 
     run_simple('localhost', 8232, application, use_debugger=True,
-               use_reloader=True, extra_files=extra_files)
+               use_reloader=True, extra_files=extra_files, threaded=True)
 
 else:
-    app.run(debug=True, port=8231, extra_files=extra_files)
+    app.run(debug=True, port=8231, extra_files=extra_files, threaded=True)
