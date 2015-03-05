d29996b56f34db2b154c10ca49f25bb8c57bcaa6
Marian Sigler <m@qjym.de>
2015-03-05 18:57:14 +0100

    backend: group entries in default_settings

--- backend/zwl/default_settings.py
+++ backend/zwl/default_settings.py
@@ -1,10 +1,7 @@
 # -*- coding: utf8 -*-
 
-# Number of seconds (int or float) to sleep before processing a request.
-# This setting can be used to test the frontend's ability to cope with slow
-# response times.
-RESPONSE_DELAY = 0
 
+# GENERAL SETTINGS
 
 # The database to connect to. For information about the url format, see
 # http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls
@@ -14,9 +11,17 @@ SQLALCHEMY_DATABASE_URI = None
 # Set this to False to use `fahrplan_zuege`.
 USE_SESSION_TIMETABLE = True
 
+# IP address or hostname and port of the clock server
+CLOCK_SERVER = ('192.168.17.5', 4711)
+
+
+# FRONTEND RELATED SETTINGS
+
 # Colors for the different train categories (see `TrainType.category`).
 TRAIN_COLOR_MAP_DARK = {'nv':'#f44', 'fv':'#48f', 'gv':'#2f6',
                         'lz':'#fff', 'sz':'#bd5', None:'#fff'}
 
-# IP address or hostname and port of the clock server
-CLOCK_SERVER = ('192.168.17.5', 4711)
+# Number of seconds (int or float) to sleep before processing a request.
+# This setting can be used to test the frontend's ability to cope with slow
+# response times.
+RESPONSE_DELAY = 0
