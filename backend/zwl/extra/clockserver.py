3c2aa885f0397e5d9e5a5fff0c65bf74e564e4ba
Marian Sigler <m@qjym.de>
2015-06-10 18:52:53 +0200

    finalize extra/clockserver

--- backend/zwl/extra/clockserver.py
+++ backend/zwl/extra/clockserver.py
@@ -4,13 +4,79 @@
     zwl.extra.clockserver
     =====================
 
-    Start a local clock server for development
+    Local clock server, usable for development
 
     :copyright: (c) 2015, Marian Sigler
     :license: GNU GPL 2.0 or later.
 """
 import socket
 from datetime import date, time, datetime
+from zwl import app
+
+class ClockServer(object):
+    """
+    Server implementing a basic variant of the clock protocol as spoken by
+    `zwl.utils.ClockConnection`.
+    """
+    def __init__(self, current_time=None, running=True):
+        print 'ClockServer', current_time, running
+        self.clock = Clock(current_time, running)
+
+    def listen(self, host=None, port=None):
+        """
+        Start the clock server.
+
+        Note that this is non-threading (as it is not needed for testing).
+
+        Host defaults to localhost, port defaults to the value set in the
+        `CLOCK_SERVER` config option.
+        """
+        if host is None:
+            host = 'localhost'
+        if port is None:
+            _, port = app.config['CLOCK_SERVER']
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Allow quick server restart. http://stackoverflow.com/a/4466035/196244
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((host, port))
+        sock.listen(2)
+
+        try:
+            clientsock = None
+            while True:
+                clientsock, clientaddress = sock.accept()
+                print 'Connection from %s:%d' % clientaddress
+                conn = clientsock.makefile('rw')
+                conn.write('100 EBuEf dev clock server\n')
+                conn.flush()
+                r = conn.readline()
+
+                if r.rstrip() != 'get 1':
+                    out = "500 unknown command"
+                else:
+                    time = self.clock.get_time().strftime('%s')
+                    state = {'running':1, 'stopped':0}[self.clock.get_state()]
+                    out = '200 1 %s 10 %d' % (time, state)
+
+                print '>', out
+                conn.write(out + '\n')
+                conn.flush()
+
+                try:
+                    # may raise EBADF if already shut down
+                    clientsock.shutdown(socket.SHUT_RDWR)
+                except socket.error, e:
+                    if e.errno != socket.EBADF:
+                        raise
+                clientsock.close()
+        finally:
+            if clientsock is not None:
+                print 'close client'
+                clientsock.close()
+            print 'close'
+            sock.close()
+
 
 class Clock(object):
     """
@@ -23,6 +89,7 @@ class Clock(object):
         if isinstance(current_time, time):
             current_time = datetime.combine(date.today(), current_time)
 
+        print 'new clock', current_time, running
         if running:
             self.state = 'running'
             self.diff = current_time - now
@@ -38,6 +105,10 @@ class Clock(object):
         if self.state == 'stopped':
             return self.current_time
         raise RuntimeError('invalid state for %r' % self)
+
+    def get_state(self):
+        assert self.state in ('running', 'stopped')
+        return self.state
 
     def start(self):
         if self.state == 'running':
@@ -65,3 +136,31 @@ class Clock(object):
             return '<ClockServer (stopped) now=%s diff=%s>' % (
                 self.current_time, self.current_time - datetime.now())
         raise RuntimeError('invalid state: %r' % self.state)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) <= 1:
+        print >>sys.stderr, 'Usage: clockserver.py ' \
+            '{realtime|%Y-%m-%dT%H:%M:%S|unixtimestamp} [stopped]'
+        sys.exit(1)
+
+    running = True
+    if len(sys.argv) > 2:
+        if sys.argv[2] == 'stopped':
+            print 'stopped'
+            running = False
+        else:
+            print >>sys.stderr, 'argument 2, if present, must be `stopped`'
+            sys.exit(1)
+
+    if sys.argv[1] == 'realtime':
+        cs = ClockServer(datetime.now(), running)
+    elif '-' in sys.argv[1]:
+        cs = ClockServer(datetime.strptime(sys.argv[1], '%FT%T'), running)
+    else:
+        cs = ClockServer(datetime.strptime(sys.argv[1], '%s'), running)
+
+    try:
+        cs.listen()
+    except KeyboardInterrupt:
+        pass # close is done by ClockServer
