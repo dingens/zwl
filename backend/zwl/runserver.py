2ba607350bee203a0a6b5132b237a4cb8db22f77
Marian Sigler <m@qjym.de>
2015-12-02 00:55:42 +0100

    backend: add public listen mode; runserver cleanup

--- backend/zwl/runserver.py
+++ backend/zwl/runserver.py
@@ -21,11 +21,11 @@ try:
 except KeyError:
     pass
 
+# Allow testing if everything still works when not running under /
 if len(sys.argv) > 1 and sys.argv[1] == 'subdir':
 
     notfound = Response('Use <a href="/zwl/">/zwl/</a>, please!', status=404)
 
-    # allows testing if everything still works when not running under /
     from werkzeug.serving import run_simple
     from werkzeug.wsgi import DispatcherMiddleware
 
@@ -36,5 +36,15 @@ if len(sys.argv) > 1 and sys.argv[1] == 'subdir':
     run_simple('localhost', 8232, application,
                use_reloader=True, extra_files=extra_files, threaded=True)
 
-else:
+# Listen on all interfaces instead of just localhost. Force-deactivate debug mode
+elif len(sys.argv) > 1 and sys.argv[1] == 'public':
+    app.run(host='0.0.0.0', port=8231, extra_files=extra_files, threaded=True, debug=False)
+
+# Normal mode
+elif len(sys.argv) == 1:
     app.run(port=8231, extra_files=extra_files, threaded=True)
+
+# Don't accept unknown command line parameters
+else:
+    print >>sys.stderr, 'unknown command'
+    sys.exit(1)
