7a04712163798b45b6c8ec80405809afe47dda9e
Marian Sigler <m@qjym.de>
2016-06-04 21:54:55 +0200

    frontend: decrease graphminwidth

--- frontend/zwl.js
+++ frontend/zwl.js
@@ -195,7 +195,7 @@ ZWL.Display.prototype = {
         graphtopmargin: 45,
         graphbottommargin: 50,
         graphhorizmargin: 70,
-        graphminwidth: 200,
+        graphminwidth: 150,
         timeaxiswidth: 80,
         horizdistance: -10,
     },
